df007b6b29460916d9746938c074a3c88da7bbc6
Alberto Carmona <carmo@aleph.engineering>
2022-05-05 01:49:52 -0400

    Register the models in the admin

--- prj_drone_drf/drone_app/admin.py
+++ prj_drone_drf/drone_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from prj_drone_drf.drone_app.models import Drone, Medication
+
 # Register your models here.
+
+admin.site.register(Drone)
+admin.site.register(Medication)
