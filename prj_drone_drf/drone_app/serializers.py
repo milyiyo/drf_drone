e6640193cda2974ebdf8124c2b9c6305e8c77824
Alberto Carmona <carmo@aleph.engineering>
2022-05-05 18:08:34 -0400

    Check drone's capacity and battery level

--- prj_drone_drf/drone_app/serializers.py
+++ prj_drone_drf/drone_app/serializers.py
@@ -21,6 +21,22 @@ class DroneSerializer(serializers.HyperlinkedModelSerializer):
         validate_fleet_size(Drone, serializers.ValidationError)
         return data
 
+    def update(self, instance, validated_data):
+        # Prevent the drone from being loaded with more weight that it can carry
+        sum_weight_medications = sum(
+            [m.weight for m in validated_data['medications']])
+        if validated_data['weight_limit'] < sum_weight_medications:
+            raise serializers.ValidationError(
+                "The sum of medications' weight can't exceed the drone's capacity")
+
+        # Prevent the drone from being in LOADING state if the battery level is **below 25%**
+        if validated_data['battery_capacity'] < 25 and validated_data['state'] == 'LOADING':
+            raise serializers.ValidationError(
+                "The drone cannot be in LOADING state if its battery level is below 25%")
+
+        super().update(instance, validated_data)
+        return instance
+
 
 class MedicationSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
