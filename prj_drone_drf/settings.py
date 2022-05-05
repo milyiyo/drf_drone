9e0ec4aad04d86dde0e89b30f399171a8863d464
Alberto Carmona <carmo@aleph.engineering>
2022-05-05 01:45:30 -0400

    Configure the images' folder

--- prj_drone_drf/settings.py
+++ prj_drone_drf/settings.py
@@ -15,6 +15,10 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Media
+MEDIA_ROOT = str(BASE_DIR)
+MEDIA_URL = '/images_med/'
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
--- prj_drone_drf/urls.py
+++ prj_drone_drf/urls.py
@@ -13,10 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
 from django.urls import include, path
 from rest_framework import routers
+
 from prj_drone_drf.drone_app import views
 
 router = routers.DefaultRouter()
@@ -27,4 +29,4 @@ urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
